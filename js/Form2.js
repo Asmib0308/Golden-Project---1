718a8852670fea38f048328bccec5965d366fcc3
Asmib0308 <78547003+Asmib0308@users.noreply.github.com>
2021-09-05 13:41:33 +0530

    Create Form2.js

--- js/Form2.js
+++ js/Form2.js
@@ -83,7 +83,7 @@ class Form2 {
       }
     }
     
-    this.submit.position(displayWidth/2 + 370, displayHeight/2 + 120);
+    this.submit.position(displayWidth/2 + 370, displayHeight/2 + 70);
     this.submit.size(130,35);
     this.submit.mousePressed(async()=>{    
       answer = new Answers (this.hero.value(),this.heroine.value(),this.song.value(),this.movie.value()) 
@@ -103,7 +103,7 @@ class Form2 {
           console.log("works")
         },60000)
     }
-    this.saveGame.position(displayWidth/2 + 370, displayHeight/2 + 160)
+    this.saveGame.position(displayWidth/2 + 370, displayHeight/2 + 100)
     this.saveGame.mousePressed(async()=>{   
       form.update(qid)
      })
@@ -138,14 +138,14 @@ class Form2 {
     textSize(25)
     text("Trivia about the movie : ",displayWidth/2 - 660, displayHeight/2 + 120)
     textSize(17);
-    text(this.hints.hint1,displayWidth/2 - 670 , displayHeight/2 + 150)
-    text(this.hints.hint2,displayWidth/2 - 670 , displayHeight/2 + 210)
+    text(this.hints.hint1,displayWidth/2 - 670 , displayHeight/2 + 160)
+    text(this.hints.hint2,displayWidth/2 - 670 , displayHeight/2 + 180)
          
     line(windowWidth/2, windowHeight/2 - 200, windowWidth/2, windowHeight/2 + 100)
     line(windowWidth/2 - 340, windowHeight/2 - 60, windowWidth/2 + 365, windowHeight/2 - 60)
    
     
-      this.hint3.position(windowWidth/2 + 370 , windowHeight/2 + 70);
+      this.hint3.position(windowWidth/2 + 370 , windowHeight/2 + 120);
       this.hint3.style("color","purple")
       this.hint3.mousePressed(()=>{
         if(points >= 15){
@@ -156,10 +156,10 @@ class Form2 {
       })
       if (this.hintFlag === true){
         textSize(15);
-        text(this.hints.hint3,windowWidth/2 + 370 , windowHeight/2 + 70)
+        text(this.hints.hint3,windowWidth/2 + 370 , windowHeight/2 + 120)
       }
             
-      this.hint4.position(windowWidth/2 + 370 , windowHeight/2 + 100)  ;
+      this.hint4.position(windowWidth/2 + 370 , windowHeight/2 + 160)  ;
       this.hint4.style("color","purple")
       this.hint4.mousePressed(()=>{ 
         if(points>= 15){       
@@ -170,7 +170,7 @@ class Form2 {
       })
       if (this.hintFlag2 === true){
         textSize(15);
-        text(this.hints.hint4,windowWidth/2 + 370 , windowHeight/2 + 100)            
+        text(this.hints.hint4,windowWidth/2 + 370 , windowHeight/2 + 160)            
         }
       
   }
